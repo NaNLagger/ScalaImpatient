173f59ef5007778d8cc924868e40776117367be2
NaNLagger <stepan.lyashenko@gmail.com>
2015-03-19 00:26:46 +0600

    adding 3 exercises in Lesson15

--- src/main/lesson15/Lesson15.scala
+++ src/main/lesson15/Lesson15.scala
@@ -1,22 +1,36 @@
 package main.lesson15
 
+import com.sun.istack.internal.NotNull
 import main.Lesson2
+
+import scala.annotation.varargs
+import scala.beans.BeanProperty
 
 /**
  * Created by NaNLagger on 18.03.15.
  * @author Stepan Lyashenko
  */
 object Lesson15 extends App {
+  //Ex 1. see Lesson2Test
 
+  //Ex 2.
   {
-    import org.junit.Assert._
-    import org.junit.Test
-    class Lesson2Test {
-      @Test
-      def testLesson14(): Unit = {
-        assertEquals(Lesson2.pow(2,10), 1020)
+    @Deprecated class WhippingBoy @Deprecated() (@NotNull message: String = "Oh no, stop it!") {
+      @BeanProperty var count_shot: Int = 10
+      def shot() = {
+        if (count_shot > 0) {
+          count_shot -= 1 : @Deprecated
+          println(message)
+        } else {
+          println("I'm already dead")
+        }
       }
     }
+  }
+
+  //Ex 3.
+  {
+    @varargs def sum(args: Int*) = args.sum
   }
 
 
